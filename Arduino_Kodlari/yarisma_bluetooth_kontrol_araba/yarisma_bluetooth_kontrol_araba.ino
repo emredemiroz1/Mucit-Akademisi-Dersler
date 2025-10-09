1e1dfbd08dc127fd62bca4872b54393641b7982b
Emre DEMİRÖZ <emredemiroz173@gmail.com>
2025-10-09 19:43:45 +0300

    Update yarisma_bluetooth_kontrol_araba.ino

--- Arduino_Kodlari/yarisma_bluetooth_kontrol_araba/yarisma_bluetooth_kontrol_araba.ino
+++ Arduino_Kodlari/yarisma_bluetooth_kontrol_araba/yarisma_bluetooth_kontrol_araba.ino
@@ -1,14 +1,8 @@
-#include <SoftwareSerial.h>
-
-// Bluetooth RX, TX pinleri
-SoftwareSerial BT(2, 3); // RX, TX (HC-05 ile bağlantı)
-
 // Motor pinlerini tanımlıyoruz
 #define SOL_MOTOR1 5   
 #define SOL_MOTOR2 6   
 #define SAG_MOTOR1 9   
 #define SAG_MOTOR2 10 
- 
 
 char komut; // Telefon'dan gelen komut
 
@@ -19,20 +13,18 @@ void setup() {
   pinMode(SAG_MOTOR1, OUTPUT);
   pinMode(SAG_MOTOR2, OUTPUT);
 
-  // Seri haberleşme
-  Serial.begin(9600);   // Arduino Seri Monitör
-  BT.begin(9600);       // Bluetooth haberleşme
+  // Seri haberleşme (Hem Bluetooth hem USB Serial aynı hattan gelir)
+  Serial.begin(9600);   
   Serial.println("Bluetooth araba hazır. Komut bekleniyor...");
 }
 
 void loop() {
-  if (BT.available()) {      // Bluetooth'tan veri geldiyse
-    komut = BT.read();       // Karakteri oku
-    Serial.println(komut);   // Seri monitöre yaz
+  if (Serial.available()) {      // Bluetooth'tan veri geldiyse
+    komut = Serial.read();       // Karakteri oku
+    Serial.println(komut);       // Seri monitöre yaz
 
     // Gelen komuta göre hareket et
-    if (komut == 'F') 
-    ileri();    // Forward
+    if (komut == 'F') ileri();    // Forward
     else if (komut == 'B') geri(); // Backward
     else if (komut == 'R') sag();  // Right
     else if (komut == 'L') sol();  // Left
